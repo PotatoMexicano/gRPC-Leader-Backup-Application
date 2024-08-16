1cf8428d69077a18edd8361bf75f87424e854717
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-08-16 11:34:16 -0300

    [feat] Adicionado proto.

---
+++ LeaderWorker/Program.cs
@@ -0,0 +1,145 @@
+using Grpc.Net.Client;
+using GrpcLeaderBackup;
+using LeaderWorker.Services;
+
+public class BackupService
+{
+    private readonly String _leaderIp;
+    private readonly HttpClient _httpClient;
+    private static Boolean _leaderOnline = true;
+    public String? GuidLeader;
+
+    public BackupService(String leaderIp)
+    {
+        _leaderIp = leaderIp;
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<Boolean> CheckLeader()
+    {
+        try
+        {
+            using GrpcChannel channel = GrpcChannel.ForAddress($"http://{_leaderIp}:5000");
+            HeartbeatService.HeartbeatServiceClient client = new HeartbeatService.HeartbeatServiceClient(channel);
+
+            Console.Write($"Heartbeat solicitado às {DateTime.Now}: ");
+
+            HeartbeatResponse reply = await client.CheckAsync(new HeartbeatRequest { Message = "Ping" });
+
+            Console.WriteLine(reply.IsAlive ? "Leader" : "");
+
+            return reply.IsAlive;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void AssumeLeadership()
+    {
+        // Executar o trabalho do líder a cada 10 segundos
+        while (!_leaderOnline)
+        {
+            try
+            {
+                HeartbeatServiceImpl.LeaderState state = new HeartbeatServiceImpl.LeaderState { GuidLeader = GuidLeader };
+                HeartbeatServiceImpl.PerformLeaderWork(state);
+                Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+
+                _leaderOnline = CheckLeader().Result;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+
+    public async Task MonitorLeader()
+    {
+        while (true)
+        {
+            try
+            {
+                Boolean isLeaderAlive = await CheckLeader();
+                _leaderOnline = isLeaderAlive;
+                if (!isLeaderAlive)
+                {
+                    //Console.WriteLine("Líder caiu, assumindo liderança.");
+                    AssumeLeadership();
+                }
+            }
+            catch
+            {
+                //Console.WriteLine("Erro na comunicação com o líder, assumindo liderança.");
+                AssumeLeadership();
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+        }
+
+    }
+
+}
+
+public class Program
+{
+    public static async Task Main(String[] args)
+    {
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        Boolean isLeader = config.GetValue<Boolean>("Leader");
+        String leaderIp = config.GetValue<String>("LeaderIp");
+
+
+        if (isLeader)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+            Console.WriteLine("Iniciando como líder...");            
+            HeartbeatServiceImpl.LeaderState state = new HeartbeatServiceImpl.LeaderState { GuidLeader = "Leader" };
+            HeartbeatServiceImpl.StartLeaderWork(state);
+            await host.RunAsync();
+        }
+        else
+        {
+            Console.WriteLine("Iniciando como backup...");            
+            HeartbeatServiceImpl.LeaderState state = new HeartbeatServiceImpl.LeaderState { GuidLeader = "Backup1" };
+            BackupService backupService = new BackupService(leaderIp)
+            {
+                GuidLeader = state.GuidLeader
+            };
+            await backupService.MonitorLeader();
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(String[] args)
+    {
+        return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseUrls("http://0.0.0.0:5000");
+        webBuilder.UseStartup<Startup>();
+    });
+    }
+}
+
+public class Startup()
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddGrpc();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcService<HeartbeatServiceImpl>();
+        });
+    }
+}
---
+++ LeaderWorker/Services/HeartbeatServiceImpl.cs
@@ -0,0 +1,61 @@
+﻿using Grpc.Core;
+using GrpcLeaderBackup;
+
+namespace LeaderWorker.Services;
+
+public class HeartbeatServiceImpl : HeartbeatService.HeartbeatServiceBase
+{
+    private static Timer _timer;
+
+    public override Task<HeartbeatResponse> Check(HeartbeatRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HeartbeatResponse
+        {
+            IsAlive = true
+        });
+    }
+
+    public class LeaderState
+    {
+        public String GuidLeader { get; set; }
+    }
+
+    public static void StartLeaderWork(Object state)
+    {
+        _timer = new Timer(PerformLeaderWork, state, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+    }
+
+    public static void PerformLeaderWork(Object state)
+    {
+        LeaderState? leaderState = state as LeaderState;
+
+        Int32[] numbers = GenerateRandomNumbers(20);
+        List<Int32> mmcs = numbers.Select(n => CalculateMMC(n, 3)).ToList();
+
+        String guidLeader = leaderState.GuidLeader;
+        Console.WriteLine(guidLeader);
+    }
+
+    private static Int32[] GenerateRandomNumbers(Int32 count)
+    {
+        Random random = new Random();
+        return Enumerable.Range(0, count).Select(_ => random.Next(1, 100)).ToArray();
+    }
+
+    private static Int32 CalculateMMC(Int32 a, Int32 b)
+    {
+        return Math.Abs(a * b) / CalculateMDC(a, b);
+    }
+
+    private static Int32 CalculateMDC(Int32 a, Int32 b)
+    {
+        while (b != 0)
+        {
+            Int32 temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
